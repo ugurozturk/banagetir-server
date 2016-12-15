200a44a940e66e798820a7614facf2d9796287e2
ugurozturk <tarantulaugur@hotmail.com>
2016-12-15 06:18:47 +0300

    Bayile için Post Eklendi

--- index.php
+++ index.php
@@ -5,6 +5,8 @@ use Phalcon\Mvc\Micro;
 use Phalcon\Di\FactoryDefault;
 use Phalcon\Db\Adapter\Pdo\Mysql as PdoMysql;
 
+use Phalcon\Http\Response;
+
 // Use Loader() to autoload our model
 $loader = new Loader();
 
@@ -100,15 +102,107 @@ $app->get(
 // Primary Keye bağlı bayiyi getir
 $app->get(
     "/api/bayiler/{id:[0-9]+}",
-    function ($id) {
+    function ($id) use ($app) {
+        $phql = "SELECT * FROM Models\\Verilerim\\Bayiler WHERE bayi_id = :id:";
 
+        $bayi = $app->modelsManager->executeQuery(
+            $phql,
+            [
+                "id" => $id,
+            ]
+        )->getFirst();
+
+
+
+        // Create a response
+        $response = new Response();
+
+        if ($bayi === false) {
+            $response->setJsonContent(
+                [
+                    "status" => "NOT-FOUND"
+                ]
+            );
+        } else {
+            $response->setJsonContent(
+                [
+                    "status" => "FOUND",
+                    "data"   => [
+                        "id"   => $bayi->bayi_id,
+                        "name" => $bayi->bayi_adi
+                    ]
+                ]
+            );
+        }
+
+        return $response;
     }
 );
 
 // Yeni bir bayi ekle
 $app->post(
     "/api/bayiler",
-    function () {
+    function () use ($app) {
+
+        $bayi = $app->request->getJsonRawBody();
+
+        $phql = "INSERT INTO Models\\Verilerim\\Bayiler 
+        (bayi_kullaniciadi, bayi_sifre, bayi_adi, bayi_tel, bayi_email, bayi_adres, bayi_adreskodu, vergi_numarasi, aktif, kayit_tarihi) VALUES 
+        (:bayi_kullaniciadi:, :bayi_sifre:, :bayi_adi:, :bayi_tel:, :bayi_email:, :bayi_adres:, :bayi_adreskodu:, :vergi_numarasi:, :aktif:, :kayit_tarihi:)";
+
+
+        $status = $app->modelsManager->executeQuery(
+            $phql,
+            [
+                "bayi_kullaniciadi" => $bayi->bayi_kullaniciadi,
+                "bayi_sifre"   => $bayi->bayi_sifre,
+                "bayi_adi" => $bayi->bayi_adi,
+                "bayi_tel"   => $bayi->bayi_tel,
+                "bayi_email" => $bayi->bayi_email,
+                "bayi_adres"   => $bayi->bayi_adres,
+                "bayi_adreskodu" => $bayi->bayi_adreskodu,
+                "vergi_numarasi"   => $bayi->vergi_numarasi,
+                "aktif" => $bayi->aktif,
+                "kayit_tarihi"   => $bayi->kayit_tarihi
+            ]
+        );
+
+        // Create a response
+        $response = new Response();
+
+        // Check if the insertion was successful
+        if ($status->success() === true) {
+            // Change the HTTP status
+            $response->setStatusCode(201, "Created");
+
+            $bayi->bayi_id = $status->getModel()->bayi_id;
+
+            $response->setJsonContent(
+                [
+                    "status" => "OK",
+                    "data"   => $bayi,
+                ]
+            );
+        } else {
+            // Change the HTTP status
+            $response->setStatusCode(409, "Conflict");
+
+            // Send errors to the client
+            $errors = [];
+
+            foreach ($status->getMessages() as $message) {
+                $errors[] = $message->getMessage();
+            }
+
+            $response->setJsonContent(
+                [
+                    "status"   => "ERROR",
+                    "messages" => $errors,
+                ]
+            );
+        }
+
+        return $response;
 
     }
 );
